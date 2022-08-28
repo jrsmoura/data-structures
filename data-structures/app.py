6f3873f008eb1236264e7dc4d4fec8afc0d2b8e0
J. R. Steiner <jrsteiner78@gmail.com>
2022-08-28 11:26:22 -0300

    Initial commit

---
+++ data-structures/app.py
@@ -0,0 +1,58 @@
+from binary_trees.tree import Node, Tree
+
+
+def main():
+    node = Node(10)
+
+    # A rede é do tipo binária, então os maiores valores ficam a direita
+    # enquanto os menorer, a esquerda.
+    # node.left = Node(5)
+    # node.right = Node(10)
+    #
+    # node.left.left = Node(2)
+    # node.left.right = Node(6)
+    #
+    # node.right.left = Node(13)
+    # node.right.right = Node(20)
+
+    # print(node.data)
+    # print(node.right.right.data)
+
+    # my_tree = Tree(node, "My Tree")
+    #
+    # print(my_tree.name)
+    # print(my_tree.root.left.data)
+    # print(my_tree.root.right.right.data)
+    #
+    # found = my_tree.search(220)
+    # print(found)
+
+    tree = Tree(Node(50), "Transversal Tree")
+    tree.root.left = Node(25)
+    tree.root.right = Node(75)
+    tree.root.left.left = Node(10)
+    tree.root.left.right = Node(35)
+    tree.root.left.right.left = Node(30)
+    tree.root.left.right.right = Node(42)
+    tree.root.left.left.left = Node(5)
+    tree.root.left.left.right = Node(13)
+    tree.root.left.left.left.left = Node(2)
+
+    print("Transverse Pre-Order")
+    tree.transverse_pre_order()
+
+    print("Transverse In-Order")
+    tree.transverse_in_order()
+
+    print("Transverse Post-Order")
+    tree.transverse_post_order()
+
+    print("="*20)
+    print(tree.height())
+
+    tree = Tree(Node(50), "Árvore apenas com a raiz")
+    print(tree.height())
+
+
+if __name__ == "__main__":
+    main()
---
+++ data-structures/binary_trees/tree.py
@@ -0,0 +1,92 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def search(self, target):
+        """Search for a specific node in a tree using binary search.
+           Uses recursion
+        Rules:
+
+
+        :param target: the node to be searched
+        :return: the target node
+        """
+
+        if self.data == target:
+            print("Found it")
+            return self
+
+        if self.left and self.data > target:
+            print(f"left: {self.data}")
+            return self.left.search(target)
+
+        if self.right and self.data < target:
+            print(f"right:{self.data}")
+            return self.right.search(target)
+
+        print("Value is not in tree")
+
+    def transverse_pre_order(self):
+        print(self.data)
+        if self.left:
+            self.left.transverse_pre_order()
+        if self.right:
+            self.right.transverse_pre_order()
+
+    def transverse_in_order(self):
+        if self.left:
+            self.left.transverse_pre_order()
+        print(self.data)
+        if self.right:
+            self.right.transverse_pre_order()
+
+    def transverse_post_order(self):
+        if self.left:
+            self.left.transverse_pre_order()
+        if self.right:
+            self.right.transverse_pre_order()
+        print(self.data)
+
+    def height(self, h=0):
+        left_height = self.left.height(h+1) if self.left else h
+        right_height = self.right.height(h+1) if self.right else h
+
+        return max(left_height, right_height)
+
+    def get_nodes_at_depth(self, depth, nodes=[]):
+        if nodes is None:
+            nodes = []
+        if depth == 0:
+            nodes.append(self.data)
+            return nodes
+        if self.left:
+            self.left.get_nodes_at_depth(depth-1, nodes)
+        if self.right:
+            self.right.get_nodes_at_depth(depth-1, nodes)
+        return nodes
+
+
+class Tree:
+    def __init__(self, root, name=""):
+        self.root = root
+        self.name = name
+
+    def search(self, target):
+        return self.root.search(target)
+
+    def transverse_pre_order(self):
+        return self.root.transverse_pre_order()
+
+    def transverse_in_order(self):
+        return self.root.transverse_in_order()
+
+    def transverse_post_order(self):
+        return self.root.transverse_post_order()
+
+    def height(self):
+        return self.root.height()
+
+    def get_nodes_at_depth(self, depth):
+        return self.root.get_nodes_at_depth(depth)
